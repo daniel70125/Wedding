6814af7785eb47251be2ae04f9f95fba63a61c1c
daniel70125 <dwrighttt504@gmail.com>
2021-06-11 15:05:19 -0500

    Updated

--- index.js
+++ index.js
@@ -57,6 +57,37 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-
 // End of slideshow JS
+
+// Countdown Timer
+
+// Set the date we're counting down to
+var countDownDate = new Date("June 26, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("cd-days").innerHTML = `<p>${days}</p> Days`;
+  document.getElementById("cd-hours").innerHTML = `<p>${hours}</p> Hours`;
+  document.getElementById("cd-minutes").innerHTML = `<p>${minutes}</p> Minutes`;
+  document.getElementById("cd-seconds").innerHTML = `<p>${seconds}</p> Seconds`;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
